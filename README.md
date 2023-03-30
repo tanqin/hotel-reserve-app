2243974beabb6f28bd1d6a0b7f8fd599f138d56f
tanqin <cn.tanqin@qq.com>
2023-03-30 19:46:31 +0800

    chore: 项目初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+# hotel-reserve-app『酒店预订APP』
---
+++ src/pages/greet/greet.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <view class="greet">
+    欢迎页
+  </view>
+</template>
+
+<style scoped>
+
+</style>
